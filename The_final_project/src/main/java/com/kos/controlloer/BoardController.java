e0816fc8c9964f45323c75df4d97cac7de82beba
junpublic <62416022+junpublic@users.noreply.github.com>
2020-07-27 10:07:04 +0900

    보드컨트롤러 메퍼 수정 -정준

--- The_final_project/src/main/java/com/kos/controlloer/BoardController.java
+++ The_final_project/src/main/java/com/kos/controlloer/BoardController.java
@@ -103,13 +103,66 @@ public class BoardController {
 		// System.out.println(result.getTitle()+"######################");
 		System.out.println(result.getBoardView() + "뷰카운트");
 		// 다음 페이지 지정
+		if(vo.getB_boardname().equals("nongsain")) {
+			mv.setViewName("viewboardNongsain");
+			mv.addObject("board", result);
+
+			return mv;
+		}
 		mv.setViewName("viewboard");
 		mv.addObject("board", result);
 
 		return mv;
 
 	}
-
+//	@RequestMapping("imageUpload.do")
+//	public void imageUpload(HttpServletRequest request, HttpServletResponse response,
+//			@RequestParam MultipartFile upload) throws Exception{
+//
+//		//http hearder 설정
+//		response.setCharacterEncoding("utf-8");
+//		response.setContentType("text/html;charset=utf-8");
+//		//http body
+//		//업로드한 파일 이름
+//		String fileName = upload.getOriginalFilename();
+//		
+//
+//		//바이트 배열로 변환
+//		byte[] bytes = upload.getBytes();
+//		//이미지를 업로드할 디렉토리(배포 경로로 설정)
+//
+//		String uploadPath = "C:\\Users\\uoygn\\Desktop\\WebDevelop\\Shop\\blog\\src\\main\\webapp\\resources\\uploadimage";
+//
+//		
+//
+//		//서버에 저장됨
+//
+//		OutputStream out = new FileOutputStream(new File(uploadPath + fileName)); //java.io
+//
+//		out.write(bytes);
+//
+//		String callback = request.getParameter("CKEditorFuncNum");
+//
+//		
+//
+//		PrintWriter printWriter = response.getWriter();
+//
+//		String fileUrl = request.getContextPath() + "/uploadimage/" + fileName;
+//
+//		System.out.println("fileUrl: " + fileUrl);
+//
+//		printWriter.println("<script>window.parent.CKEDITOR.tools.callFunction("
+//
+//		+callback+",'"+fileUrl+"','이미지가 업로드 되었습니다.')" + "</script>");
+//
+//		
+//
+//		//스트림닫기
+//
+//		printWriter.flush();
+//
+//
+//	}
 	// 이미지 업로드에서 사용하는 컨트롤러
 	@RequestMapping(value="fileupload.do", method=RequestMethod.POST)
 	@ResponseBody
@@ -165,5 +218,35 @@ public class BoardController {
 		return null;
 	}	
 	
+	/*
+	 * @RequestMapping(value=".do", method=RequestMethod.POST)
+	 * 
+	 * @ResponseBody public String fileUpload(HttpServletRequest req,
+	 * HttpServletResponse resp, MultipartHttpServletRequest multiFile) throws
+	 * Exception { JsonObject json = new JsonObject(); PrintWriter printWriter =
+	 * null; OutputStream out = null; MultipartFile file =
+	 * multiFile.getFile("upload"); if(file != null){ if(file.getSize() > 0 &&
+	 * StringUtils.isNotBlank(file.getName())){
+	 * if(file.getContentType().toLowerCase().startsWith("image/")){ try{ String
+	 * fileName = file.getName(); byte[] bytes = file.getBytes(); //이부분 getsession
+	 * 먼저 해줘야함 servlet을 사용하지 않는 jsp는 이런식으로 먼저 세션을 가져와야함 String uploadPath =
+	 * req.getSession().getServletContext().getRealPath("/img");
+	 * 
+	 * File uploadFile = new File(uploadPath); if(!uploadFile.exists()){
+	 * uploadFile.mkdirs(); } fileName = UUID.randomUUID().toString(); uploadPath =
+	 * uploadPath + "/" + fileName; out = new FileOutputStream(new
+	 * File(uploadPath)); out.write(bytes);
+	 * 
+	 * printWriter = resp.getWriter(); resp.setContentType("text/html"); String
+	 * fileUrl = req.getContextPath() + "/img/" + fileName;
+	 * 
+	 * // json 데이터로 등록 // {"uploaded" : 1, "fileName" : "test.jpg", "url" :
+	 * "/img/test.jpg"} // 이런 형태로 리턴이 나가야함. json.addProperty("uploaded", 1);
+	 * json.addProperty("fileName", fileName); json.addProperty("url", fileUrl);
+	 * 
+	 * printWriter.println(json); }catch(IOException e){ e.printStackTrace();
+	 * }finally{ if(out != null){ out.close(); } if(printWriter != null){
+	 * printWriter.close(); } } } } } return null; }
+	 */
 
 }
