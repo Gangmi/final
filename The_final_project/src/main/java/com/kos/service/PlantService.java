2a2ae047f923deb05a3f39fca4a74f4069e6f71b
kimgyuteak <spdlxmghs@naver.com>
2020-07-29 16:49:25 +0900

    식물사전 추가
    식물사전 추가

--- The_final_project/src/main/java/com/kos/service/PlantService.java
+++ The_final_project/src/main/java/com/kos/service/PlantService.java
@@ -1,10 +1,12 @@
 package com.kos.service;
 
 import java.util.List;
+
+import com.kos.vo.PagingVO;
 import com.kos.vo.PlantVO;
 
 public interface PlantService {
-	
-	public List<PlantVO> selectPlant(PlantVO vo);
-	
-}
+	 
+	public List<PlantVO> selectPlant(PagingVO vo); 
+}    
+       
--- The_final_project/src/main/java/com/kos/service/PlantServiceImpl.java
+++ The_final_project/src/main/java/com/kos/service/PlantServiceImpl.java
@@ -6,17 +6,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kos.dao.PlantDaoImpl;
+import com.kos.vo.BoardVO;
+import com.kos.vo.PagingVO;
 import com.kos.vo.PlantVO; 
 
 @Service
 public class PlantServiceImpl implements PlantService{
 
-	@Autowired
+	@Autowired 
 	PlantDaoImpl dao;
 
 	@Override
-	public List<PlantVO> selectPlant(PlantVO vo) {
-		return dao.selectPlant(vo);
-	} 
+	public List<PlantVO> selectPlant(PagingVO vo) {
+		
+		int total = dao.getPlantCount(vo);
+		System.out.println(total);	//330
+		PagingVO page = new PagingVO(total, vo.getNowpage(), vo.getCntPerPage());
+		System.out.println(page.getStart());	//-19 
+		System.out.println(page.getEnd());		// 0
+		System.out.println(page.getLastPage());  //17 
+		System.out.println(page.getNowpage());   // 1
+		List<PlantVO> result = dao.selectPlant(page);  
+		// nowpage멤버변수를 이용해서 total page를 저장
+		if (result.size() > 0) {  
+			result.get(0).setNowpage(page.getLastPage());
+		}  
+   
+		return result; 
+	}
  
 } 
+ 
